021deca7630210f056cdd65149f6cd5618da4bc1
Trevor West <twest@expectancylearning.com>
2021-12-18 21:40:47 -0600

    Feature 2
    Add all and remove all buttons on category page

--- README.md
+++ README.md
@@ -24,3 +24,24 @@ File 1 was used to insert the second image's `srcset` as a data attribute.
 
 File 2 replaces the current `srcset` with the one in the data attribute on `mouseenter` and reverts it on `mouseleave`
 
+### Feature 2
+
+This feature adds an 'Add All to Cart' button and a 'Remove All Items' button.
+
+Modified Files:
+
+1. assets\js\theme\category.js
+
+2. templates\components\products\card.html
+
+3. templates\components\category\product-listing.html
+
+4. lang\\*.json
+
+File 1 houses all button logic (using the Storefront API) and notifications. I used `sweetalert` to notify the user of added and removed cart items. This file also ties into Event Hooks to update the cart quantity.
+
+File 2 added a `product-id` data attribute to the card template for easy retrieval. 
+
+File 3 has the button HTML
+
+File 4 is all updated translation files. I used Google Translate for *most* of them.
--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -3,6 +3,7 @@ import CatalogPage from './catalog';
 import compareProducts from './global/compare-products';
 import FacetedSearch from './common/faceted-search';
 import { createTranslationDictionary } from '../theme/common/utils/translations-utils';
+import swal from './global/sweet-alert';
 
 export default class Category extends CatalogPage {
     constructor(context) {
@@ -14,6 +15,125 @@ export default class Category extends CatalogPage {
         $element.attr({
             role: roleType,
             'aria-live': ariaLiveStatus,
+        });
+    }
+
+    getCart() {
+        return fetch('/api/storefront/cart', {
+            credentials: 'include'
+        }).then(function (response) {
+            return response.json();
+        });
+    }
+
+    addAllToCart(event) {
+        // get all products within this category
+        const categoryId = this.context.categoryId;
+
+        // should use bigcommerce api to fetch due to categoryProductsPerPage limit
+        let productIds = [];
+        $('ul.productGrid li.product .card').each((i, product) => {
+            productIds.push($(product).data('productId'));
+        });
+
+        // get the cart quantity so we can properly update the cart's counter
+        let cartQuantity = localStorage.getItem('cart-quantity');
+        if (cartQuantity) {
+            cartQuantity = parseInt(cartQuantity, 10) + productIds.length;
+
+            // update local storage since they so nicely gave it to us
+            localStorage.setItem('cart-quantity', cartQuantity);
+        } else {
+            cartQuantity = 0;
+        }
+
+        let requestsQueue = [];
+
+        // add each item to the cart
+        for (let i = 0; i < productIds.length; i++) {
+            // this method will create the cart if it doesn't exist
+            let request = $.get(`/cart.php?action=add&product_id=${productIds[i]}`);
+
+            requestsQueue.push(request);
+
+            // use event hooks
+            hooks.emit('cart-item-add', event, event.target);
+        }
+
+        // once all ajax requests return
+        $.when(requestsQueue).done(function (args) {
+            // added items, so show the button
+            $('[data-button-type="remove-all-cart"]').show();
+
+            // use event hook to update cart's quantity badge
+            const $body = $('body');
+            $body.trigger('cart-quantity-update', cartQuantity);
+
+            // notify user of event (could translate text here)
+            const itemTranslation = productIds.length === 1 ? 'item' : 'items';
+            swal.fire({
+                position: 'top-end',
+                text: `Added ${productIds.length} ${itemTranslation} to cart`,
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        });
+    }
+
+    removeAllFromCart() {
+        this.getCart().then((carts) => {
+            let cartId = '';
+            if (carts.length > 0 && carts[0].hasOwnProperty('id')) {
+                cartId = carts[0].id;
+            }
+
+            if (cartId.length > 0) {
+                this.deleteFromCart(cartId).then(function (response) {
+                    // no items, so hide the button
+                    $('[data-button-type="remove-all-cart"]').hide();
+
+                    // use event hook to reset cart's quantity badge
+                    const $body = $('body');
+                    $body.trigger('cart-quantity-update', 0);
+
+                    // notify user of event (could translate text here)
+                    swal.fire({
+                        position: 'top-end',
+                        text: 'Cart has been emptied',
+                        icon: 'success',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }).catch(err => {
+                    swal.fire({
+                        position: 'top-end',
+                        text: 'Failed to empty cart',
+                        icon: 'error'
+                    });
+                });
+            } else {
+                // if they see this then they used the inspect tool to show the button
+                swal.fire({
+                    position: 'top-end',
+                    text: 'Cart already empty',
+                    icon: 'error',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        })
+    }
+
+    deleteFromCart(cartId) {
+        return fetch(`/api/storefront/carts/${cartId}`, {
+            method: "DELETE",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(function (response) {
+            return response;
         });
     }
 
@@ -38,9 +158,18 @@ export default class Category extends CatalogPage {
     }
 
     onReady() {
+        // show remove all button if cart has items (default is hidden)
+        const cartQuantity = localStorage.getItem('cart-quantity');
+        if (cartQuantity && parseInt(cartQuantity, 10) > 0) {
+            $('[data-button-type="remove-all-cart"]').show();
+        }
+
         this.arrangeFocusOnSortBy();
 
         $('[data-button-type="add-cart"]').on('click', (e) => this.setLiveRegionAttributes($(e.currentTarget).next(), 'status', 'polite'));
+
+        $('[data-button-type="add-all-cart"]').on('click', (e) => this.addAllToCart(e));
+        $('[data-button-type="remove-all-cart"]').on('click', (e) => this.removeAllFromCart());
 
         $('ul.productGrid li.product .card-figure').on('mouseenter', (e) => this.setToSecondImage($(e.currentTarget)));
         $('ul.productGrid li.product .card-figure').on('mouseleave', (e) => this.resetToFirstImage($(e.currentTarget)));
